cdc6ff752ac1468c7a70bc25aebb7775c8b133c9
Paul balderas <xolostj82@gmail.com>
2026-02-18 16:43:25 -0800

    Create App component for Pi Pro Hire

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>🚀 Pi Pro Hire</h1>
+      <p>Decentralized Freelance Marketplace Powered by Pi</p>
+    </div>
+  )
+}
